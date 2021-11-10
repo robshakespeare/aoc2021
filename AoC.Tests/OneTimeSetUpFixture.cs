e99f3a8e6625ca607cd25a06dc66589ead5c0240
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-11-10 22:44:25 +0000

    Added StringExtensions and tests project

---
+++ AoC.Tests/OneTimeSetUpFixture.cs
@@ -0,0 +1,51 @@
+global using FluentAssertions;
+global using NUnit.Framework;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AoC.Tests
+{
+    [SetUpFixture]
+    public static class OneTimeSetUpFixture
+    {
+        private static AnsiEscapeCodeCleanerTextWriter? _textWriter;
+
+        /// <summary>
+        /// Ran only once, before all tests, but after the test discovery phase.
+        /// </summary>
+        [OneTimeSetUp]
+        public static void RunOnceBeforeAllTests()
+        {
+            _textWriter = new AnsiEscapeCodeCleanerTextWriter();
+            Console.SetOut(_textWriter);
+        }
+
+        /// <summary>
+        /// Ran only once, after all tests.
+        /// </summary>
+        [OneTimeTearDown]
+        public static void RunOnceAfterAllTests()
+        {
+            _textWriter?.Dispose();
+        }
+
+        private sealed class AnsiEscapeCodeCleanerTextWriter : TextWriter
+        {
+            private static readonly Regex AnsiEscapeCodeRegex = new("\x001B.+?m", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+            public AnsiEscapeCodeCleanerTextWriter() => NewLine = "\n";
+
+            public override Encoding Encoding { get; } = Encoding.Default;
+
+            public override void Write(string? value)
+            {
+                if (value != null)
+                {
+                    TestContext.Progress.Write(AnsiEscapeCodeRegex.Replace(value, ""));
+                }
+            }
+
+            public override void Write(char value) => TestContext.Progress.Write(value);
+        }
+    }
+}
---
+++ AoC.Tests/StringExtensionsTests.cs
@@ -0,0 +1,135 @@
+namespace AoC.Tests;
+
+public class StringExtensionsTests
+{
+    // Carriage Return = \r
+    // Line Feed = \n
+
+    [Test]
+    public void NormalizeLineEndings_DoesNormalize_LineFeed()
+    {
+        // ACT
+        var result = "test\nvalue\nhere".NormalizeLineEndings();
+
+        // ASSERT
+        result.Split(Environment.NewLine).Should().BeEquivalentTo(
+            "test",
+            "value",
+            "here");
+    }
+
+    [Test]
+    public void NormalizeLineEndings_DoesNormalize_CarriageReturn()
+    {
+        // ACT
+        var result = "test\rvalue\rhere".NormalizeLineEndings();
+
+        // ASSERT
+        result.Split(Environment.NewLine).Should().BeEquivalentTo(
+            "test",
+            "value",
+            "here");
+    }
+
+    [Test]
+    public void NormalizeLineEndings_DoesNormalize_CarriageReturnLineFeed()
+    {
+        // ACT
+        var result = "test\r\nvalue\r\nhere".NormalizeLineEndings();
+
+        // ASSERT
+        result.Split(Environment.NewLine).Should().BeEquivalentTo(
+            "test",
+            "value",
+            "here");
+    }
+
+    [Test]
+    public void NormalizeLineEndings_DoesNormalize_Mixture()
+    {
+        // ACT
+        var result = "test\r\n\nvalue\r\r\n\nhere".NormalizeLineEndings();
+
+        // ASSERT
+        result.Split(Environment.NewLine).Should().BeEquivalentTo(
+            "test",
+            "",
+            "value",
+            "",
+            "",
+            "here");
+    }
+
+    [Test]
+    public void NormalizeLineEndings_DoesNormalize_MultipleBlankLines()
+    {
+        // ACT
+        var result = "\r\n\r\n\r\n\n\n\n\r\r\r".NormalizeLineEndings();
+
+        // ASSERT
+        result.Should().BeEquivalentTo(string.Join("", Enumerable.Repeat(Environment.NewLine, 9)));
+    }
+
+    [Test]
+    public void NormalizeLineEndings_WhenInputNull_ReturnsEmptyString()
+    {
+        // ACT
+        var result = ((string?)null).NormalizeLineEndings();
+
+        // ASSERT
+        result.Should().BeEmpty();
+    }
+
+    [Test]
+    public void ReadLines_DoesParseEachLineOfStringIntoArrayElements_And_DoesNormalizeLineEndings()
+    {
+        // ACT
+        var result = "hello\nworld\r\n\r\nthis\ris\r\na\ntest".ReadLines();
+
+        // ASSERT
+        result.Should().BeEquivalentTo(
+            "hello",
+            "world",
+            "",
+            "this",
+            "is",
+            "a",
+            "test");
+    }
+
+    [Test]
+    public void ReadLines_DoesTrimTrailingLineEndings()
+    {
+        // ACT
+        var result = "hello\r\n\r\n\n\n\r\nworld\r\n\n\n\r\n".ReadLines();
+
+        // ASSERT
+        result.Should().BeEquivalentTo(
+            "hello",
+            "",
+            "",
+            "",
+            "",
+            "world");
+    }
+
+    [Test]
+    public void ReadLines_DoesTrimTrailingLineEndings_AndResultInAnEmptyCollectionIfInputIsJustNewLines()
+    {
+        // ACT
+        var result = "\r\n\r\n\r\n\n\n\r\n".ReadLines();
+
+        // ASSERT
+        result.Should().BeEmpty();
+    }
+
+    [Test]
+    public void ReadLines_WhenInputNull_ReturnsEmptyCollection()
+    {
+        // ACT
+        var result = ((string?)null).ReadLines().ToArray();
+
+        // ASSERT
+        result.Should().BeEmpty();
+    }
+}
