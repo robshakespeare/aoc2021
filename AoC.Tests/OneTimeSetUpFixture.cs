03b73ca48dca1f1a9a8056a1c38bdb7695247a7e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-11-12 22:08:44 +0000

    Minor code tidy and improvements

--- AoC.Tests/OneTimeSetUpFixture.cs
+++ AoC.Tests/OneTimeSetUpFixture.cs
@@ -1,51 +1,17 @@
 global using FluentAssertions;
 global using NUnit.Framework;
-using System.Text;
-using System.Text.RegularExpressions;
 
-namespace AoC.Tests
+namespace AoC.Tests;
+
+[SetUpFixture]
+public static class OneTimeSetUpFixture
 {
-    [SetUpFixture]
-    public static class OneTimeSetUpFixture
+    /// <summary>
+    /// Ran only once, before all tests, but after the test discovery phase.
+    /// </summary>
+    [OneTimeSetUp]
+    public static void RunOnceBeforeAllTests()
     {
-        private static AnsiEscapeCodeCleanerTextWriter? _textWriter;
-
-        /// <summary>
-        /// Ran only once, before all tests, but after the test discovery phase.
-        /// </summary>
-        [OneTimeSetUp]
-        public static void RunOnceBeforeAllTests()
-        {
-            _textWriter = new AnsiEscapeCodeCleanerTextWriter();
-            Console.SetOut(_textWriter);
-        }
-
-        /// <summary>
-        /// Ran only once, after all tests.
-        /// </summary>
-        [OneTimeTearDown]
-        public static void RunOnceAfterAllTests()
-        {
-            _textWriter?.Dispose();
-        }
-
-        private sealed class AnsiEscapeCodeCleanerTextWriter : TextWriter
-        {
-            private static readonly Regex AnsiEscapeCodeRegex = new("\x001B.+?m", RegexOptions.Compiled | RegexOptions.CultureInvariant);
-
-            public AnsiEscapeCodeCleanerTextWriter() => NewLine = "\n";
-
-            public override Encoding Encoding { get; } = Encoding.Default;
-
-            public override void Write(string? value)
-            {
-                if (value != null)
-                {
-                    TestContext.Progress.Write(AnsiEscapeCodeRegex.Replace(value, ""));
-                }
-            }
-
-            public override void Write(char value) => TestContext.Progress.Write(value);
-        }
+        Crayon.Output.Disable();
     }
 }
--- AoC/TimingBlock.cs
+++ AoC/TimingBlock.cs
@@ -1,31 +1,30 @@
 using System.Diagnostics;
 using static Crayon.Output;
 
-namespace AoC
+namespace AoC;
+
+public class TimingBlock : IDisposable
 {
-    public class TimingBlock : IDisposable
+    private readonly string _name;
+    private readonly Stopwatch _stopwatch;
+
+    public TimingBlock(string name)
     {
-        private readonly string _name;
-        private readonly Stopwatch _stopwatch;
+        _name = name;
+        _stopwatch = Stopwatch.StartNew();
+    }
 
-        public TimingBlock(string name)
-        {
-            _name = name;
-            _stopwatch = Stopwatch.StartNew();
-        }
+    public TimeSpan Stop()
+    {
+        _stopwatch.Stop();
+        return _stopwatch.Elapsed;
+    }
 
-        public TimeSpan Stop()
-        {
-            _stopwatch.Stop();
-            return _stopwatch.Elapsed;
-        }
+    public void Dispose()
+    {
+        Stop();
 
-        public void Dispose()
-        {
-            Stop();
-
-            Console.WriteLine(Rgb(118, 118, 118).Text($"[{_name}] time taken (seconds): {_stopwatch.Elapsed.TotalSeconds:0.000}"));
-            Console.WriteLine();
-        }
+        Console.WriteLine(Rgb(118, 118, 118).Text($"[{_name}] time taken (seconds): {_stopwatch.Elapsed.TotalSeconds:0.000}"));
+        Console.WriteLine();
     }
 }
