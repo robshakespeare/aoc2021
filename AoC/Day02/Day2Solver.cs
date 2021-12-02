25dbcf9f283eadbc4f83e3cc79f1db955ddc29bc
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-02 06:40:02 +0000

    Day 2 tidy

--- AoC/Day02/Day2Solver.cs
+++ AoC/Day02/Day2Solver.cs
@@ -14,17 +14,17 @@ public class Day2Solver : SolverBase
                 new Vector2(),
                 (position, command) =>
                 {
-                    var (instruction, x) = command;
+                    var (instruction, amount) = command;
                     return position + instruction switch
                     {
-                        "forward" => new Vector2(x, 0),
-                        "down" => new Vector2(0, x),
-                        "up" => new Vector2(0, -x),
+                        "forward" => new Vector2(amount, 0),
+                        "down" => new Vector2(0, amount),
+                        "up" => new Vector2(0, -amount),
                         _ => throw new InvalidOperationException("Invalid instruction: " + instruction)
                     };
                 });
 
-        return (position.X * position.Y).Round();
+        return position.X.Round() * position.Y.Round();
     }
 
     public override long? SolvePart2(PuzzleInput input)
@@ -35,25 +35,21 @@ public class Day2Solver : SolverBase
                 new Submarine(),
                 (submarine, command) =>
                 {
-                    var (horizontal, depth, aim) = submarine;
-                    var (instruction, x) = command;
+                    var (position, aim) = submarine;
+                    var (instruction, amount) = command;
                     return instruction switch
                     {
-                        "down" => submarine with {Aim = aim + x},
-                        "up" => submarine with {Aim = aim - x},
-                        "forward" => submarine with
-                        {
-                            Horizontal = horizontal + x,
-                            Depth = depth + aim * x
-                        },
+                        "down" => submarine with {Aim = aim + amount},
+                        "up" => submarine with {Aim = aim - amount},
+                        "forward" => submarine with {Position = position + new Vector2(amount, aim * amount)},
                         _ => throw new InvalidOperationException("Invalid instruction: " + instruction)
                     };
                 });
 
-        return submarine.Horizontal * submarine.Depth;
+        return submarine.Position.X.Round() * submarine.Position.Y.Round();
     }
 
-    private readonly record struct Submarine(long Horizontal, long Depth, long Aim);
+    private readonly record struct Submarine(Vector2 Position, long Aim);
 
     private static (string instruction, long x) ParseLine(string line)
     {
