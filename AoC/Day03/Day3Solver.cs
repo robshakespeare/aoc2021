9e4697ac929e67f81cb37d3d4f0921cc23738f74
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-03 05:14:20 +0000

    day 3 p1 done

--- AoC/Day03/Day3Solver.cs
+++ AoC/Day03/Day3Solver.cs
@@ -6,7 +6,27 @@ public class Day3Solver : SolverBase
 
     public override long? SolvePart1(PuzzleInput input)
     {
-        return null;
+        var inputLines = input.ReadLines().ToArray();
+        var width = inputLines.First().Length;
+        var countOfOnes = new int[width];
+
+        foreach (var line in inputLines)
+        {
+            for (var i = 0; i < width; i++)
+            {
+                if (line[i] == '1')
+                {
+                    countOfOnes[i]++;
+                }
+            }
+        }
+
+        var midPoint = inputLines.Length / 2;
+
+        var gamma = Convert.ToInt64(string.Join("", countOfOnes.Select(count => count >= midPoint ? "1" : "0")), 2);
+        var epsilon = Convert.ToInt64(string.Join("", countOfOnes.Select(count => count < midPoint ? "1" : "0")), 2);
+
+        return gamma * epsilon;
     }
 
     public override long? SolvePart2(PuzzleInput input)
