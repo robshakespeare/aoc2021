290a72e38a45e3fbf417c18bd752cc5fd460a70d
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-04 06:19:24 +0000

    Day 4, part 1, done

--- AoC.Tests/Day04/Day4SolverTests.cs
+++ AoC.Tests/Day04/Day4SolverTests.cs
@@ -6,7 +6,25 @@ public class Day4SolverTests
 {
     private readonly Day4Solver _sut = new();
 
-    private const string ExampleInput = @"";
+    private const string ExampleInput = @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
 
     [Test]
     public void Part1Example()
@@ -15,7 +33,7 @@ public class Day4SolverTests
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(4512);
     }
 
     [Test]
