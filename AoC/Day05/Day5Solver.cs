1ce920820bffd08c3cbfcdd8627bcb2757d6c411
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-05 07:29:02 +0000

    Day 5, Attempting intersection, doesn't come out with correct counts. Maybe due to rounding issues? Maybe due to the "on line" logic being slightly of? Also, doesn't come out as much quicker!!

--- AoC/Day05/Day5Solver.cs
+++ AoC/Day05/Day5Solver.cs
@@ -21,51 +21,84 @@ public class Day5Solver : SolverBase
     private static long NumberOfPointsWhereLinesOverlap(IEnumerable<Line> lines)
     {
         lines = lines.ToArray();
+        //var points = lines.Select(l => l.Start).Concat(lines.Select(l => l.End)).ToArray();
+        //var minBounds = new Vector2(points.Min(p => p.X), points.Min(p => p.Y));
+        //var maxBounds = new Vector2(points.Max(p => p.X), points.Max(p => p.Y));
+        //var size = maxBounds - minBounds;
+        //var map = new int[size.X.Round() + 1, size.Y.Round() + 1];
 
-        var points = lines.Select(l => l.Start).Concat(lines.Select(l => l.End)).ToArray();
-
-        var minBounds = new Vector2(points.Min(p => p.X), points.Min(p => p.Y));
-        var maxBounds = new Vector2(points.Max(p => p.X), points.Max(p => p.Y));
-        var size = maxBounds - minBounds;
-
-        var map = new int[size.X.Round() + 1, size.Y.Round() + 1];
+        var intersections = new HashSet<Vector2>();
 
         foreach (var line in lines)
         {
-            var position = line.Start;
-            var end = line.End + line.NormalRounded;
-
-            while (position != end)
+            foreach (var otherLine in lines.Where(l => l != line))
             {
-                var localPos = position - minBounds;
-                map[localPos.X.Round(), localPos.Y.Round()] += 1;
-                position += line.NormalRounded;
+                var intersection = Line.Intersection(line, otherLine);
+                if (intersection != null)
+                {
+                    intersections.Add(intersection.Value.Round());
+                }
             }
         }
 
-        long count = 0;
-
-        for (var x = 0; x < map.GetLength(0); x++)
-            for (var y = 0; y < map.GetLength(1); y++)
-                if (map[x, y] > 1)
-                    count++;
-
-        return count;
+        return intersections.Count;
     }
 
     private static readonly Regex ParseLinesRegex = new(@"(?<x1>[\d]+),(?<y1>[\d]+) -> (?<x2>[\d]+),(?<y2>[\d]+)", RegexOptions.Compiled);
 
-    public record Line(Vector2 Start, Vector2 End)
+    public record Line(Vector2 Start, Vector2 End, float A, float B, float C)
     {
         public bool IsNoneDiagonal { get; } = Start.X.Round() == End.X.Round() || Start.Y.Round() == End.Y.Round();
 
-        public Vector2 NormalRounded { get; } = Vector2.Normalize(End - Start).Round();
-
         public static Line[] ParseInputToLines(PuzzleInput input) =>
             ParseLinesRegex.Matches(input.ToString())
-                .Select(match => new Line(
+                .Select(match => From(
                     new Vector2(int.Parse(match.Groups["x1"].Value), int.Parse(match.Groups["y1"].Value)),
                     new Vector2(int.Parse(match.Groups["x2"].Value), int.Parse(match.Groups["y2"].Value))))
                 .ToArray();
+
+        public static Line From(Vector2 start, Vector2 end)
+        {
+            var x1 = start.X;
+            var y1 = start.Y;
+
+            var x2 = end.X;
+            var y2 = end.Y;
+
+            var a = y2 - y1;
+            var b = x1 - x2;
+            var c = a * x1 + b * y1;
+
+            return new Line(start, end, a, b, c);
+        }
+
+        /// <summary>
+        /// From: https://www.topcoder.com/thrive/articles/Geometry%20Concepts%20part%202:%20%20Line%20Intersection%20and%20its%20Applications#LineLineIntersection
+        /// </summary>
+        public static Vector2? Intersection(Line line1, Line line2)
+        {
+            var delta = line1.A * line2.B - line2.A * line1.B;
+            if (delta == 0)
+            {
+                return null; // Lines are parallel
+            }
+
+            var x = (line2.B * line1.C - line1.B * line2.C) / delta;
+            var y = (line1.A * line2.C - line2.A * line1.C) / delta;
+
+            var point = new Vector2(x, y);
+
+            // Determine if the intersection point is actually on both of the line, rather than the extrapolation of the lines
+            // From: https://stackoverflow.com/a/17693146
+            bool IsPointOn(Line line)
+            {
+                var distAC = Vector2.Distance(line.Start, point);
+                var distBC = Vector2.Distance(line.End, point);
+                var distAB = Vector2.Distance(line.Start, line.End);
+                return Math.Abs(distAC + distBC - distAB) < 0.00001;
+            }
+
+            return IsPointOn(line1) && IsPointOn(line2) ? point : null;
+        }
     }
 }
