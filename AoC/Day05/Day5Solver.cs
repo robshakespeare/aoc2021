cfe22afbd192000d4bcc21b90423701d06cc884e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-05 06:01:49 +0000

    Refactored Day 5 to use same method

--- AoC/Day05/Day5Solver.cs
+++ AoC/Day05/Day5Solver.cs
@@ -10,35 +10,16 @@ public class Day5Solver : SolverBase
     public override long? SolvePart1(PuzzleInput input)
     {
         var noneDiagonalLines = Line.ParseInputToLines(input).Where(line => line.IsNoneDiagonal);
-
-        var map = new Dictionary<Vector2, long>();
-
-        foreach (var line in noneDiagonalLines)
-        {
-            var position = line.Start;
-            var end = line.End + line.Normal;
-
-            while (position != end)
-            {
-                if (!map.ContainsKey(position))
-                {
-                    map[position] = 1;
-                }
-                else
-                {
-                    map[position] += 1;
-                }
-                position += line.Normal;
-            }
-        }
-
-        return map.Values.Count(n => n > 1);
+        return NumberOfPointsWhereLinesOverlap(noneDiagonalLines);
     }
 
-    public override long? SolvePart2(PuzzleInput input)
-    {
-        var lines = Line.ParseInputToLines(input);
+    public override long? SolvePart2(PuzzleInput input) => NumberOfPointsWhereLinesOverlap(Line.ParseInputToLines(input));
 
+    /// <summary>
+    /// Returns the number of points where at least two of the specified lines overlap.
+    /// </summary>
+    private static long NumberOfPointsWhereLinesOverlap(IEnumerable<Line> lines)
+    {
         var map = new Dictionary<Vector2, long>();
 
         foreach (var line in lines)
@@ -68,10 +49,6 @@ public class Day5Solver : SolverBase
     public record Line(Vector2 Start, Vector2 End)
     {
         public bool IsNoneDiagonal { get; } = Start.X.Round() == End.X.Round() || Start.Y.Round() == End.Y.Round();
-
-        public float Length { get; } = Vector2.Distance(End, Start);
-
-        public Vector2 Normal { get; } = Vector2.Normalize(End - Start);
 
         public Vector2 NormalRounded { get; } = Vector2.Normalize(End - Start).Round();
 
