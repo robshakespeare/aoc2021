59060f3d1f39b1d436ddba9db6ed2e7ebd68370c
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-07 14:19:03 +0000

    Optimised Day 7 part 1, the target position is the median of the positions

--- AoC/Day07/Day7Solver.cs
+++ AoC/Day07/Day7Solver.cs
@@ -4,26 +4,24 @@ public class Day7Solver : SolverBase
 {
     public override string DayName => "The Treachery of Whales";
 
-    public override long? SolvePart1(PuzzleInput input) => GetCheapestFuelCost(CrabData.Parse(input), fuel => fuel);
+    public override long? SolvePart1(PuzzleInput input)
+    {
+        var positions = input.ToString().Split(',').Select(long.Parse).ToArray();
+        var targetPosition = positions.Median();
+
+        return positions.Select(pos => Math.Abs(targetPosition - pos)).Sum();
+    }
 
     public override long? SolvePart2(PuzzleInput input)
     {
         static int NthTriangularNumber(int n) => n * (n + 1) / 2; // From: https://math.stackexchange.com/a/60581
+        var positions = input.ToString().Split(',').Select(int.Parse).ToArray();
 
-        return GetCheapestFuelCost(CrabData.Parse(input), NthTriangularNumber);
+        return FindCheapestFuelCost(positions, NthTriangularNumber);
     }
 
-    private static int GetCheapestFuelCost(CrabData crabData, Func<int, int> fuelToCost) =>
-        (crabData.MinPosition..(crabData.MaxPosition + 1)).ToEnumerable()
-        .Select(targetPosition => crabData.Positions.Select(pos => fuelToCost(Math.Abs(targetPosition - pos))).Sum())
+    private static int FindCheapestFuelCost(int[] positions, Func<int, int> fuelToCost) =>
+        (positions.Min()..(positions.Max() + 1)).ToEnumerable()
+        .Select(targetPosition => positions.Select(pos => fuelToCost(Math.Abs(targetPosition - pos))).Sum())
         .Min();
-
-    private record CrabData(int[] Positions)
-    {
-        public int MinPosition { get; } = Positions.Min();
-
-        public int MaxPosition { get; } = Positions.Max();
-
-        public static CrabData Parse(PuzzleInput input) => new(input.ToString().Split(',').Select(int.Parse).ToArray());
-    }
 }
