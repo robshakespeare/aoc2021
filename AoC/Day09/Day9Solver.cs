0bbce62b808956d1807afb4d4bdd7dfd4a0d7eb4
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-09 08:46:10 +0000

    Day 9, part 1, done

--- AoC/Day09/Day9Solver.cs
+++ AoC/Day09/Day9Solver.cs
@@ -2,15 +2,46 @@ namespace AoC.Day09;
 
 public class Day9Solver : SolverBase
 {
-    public override string DayName => "";
+    public override string DayName => "Smoke Basin";
 
     public override long? SolvePart1(PuzzleInput input)
     {
-        return null;
+        var grid = input.ReadLines()
+            .Select((line, y) => line.Select((chr, x) => new HeightmapLocation(x, y, int.Parse($"{chr}"))).ToArray())
+            .ToArray();
+
+        var lowPoints = grid.SelectMany(locations => locations).Where(location => location.IsLowPoint(grid));
+
+        return lowPoints.Sum(location => location.RiskLevel);
     }
 
     public override long? SolvePart2(PuzzleInput input)
     {
         return null;
+    }
+
+    public record HeightmapLocation(int X, int Y, int Height)
+    {
+        public long RiskLevel { get; } = Height + 1;
+
+        public bool IsLowPoint(HeightmapLocation[][] grid) => GetAdjacentLocations(this, grid).All(l => Height < l.Height);
+
+        private static readonly (int x, int y)[] PossibleMovements = {(0, -1), (-1, 0), (1, 0), (0, 1)};
+
+        private static HeightmapLocation? SafeGetLocation(int x, int y, HeightmapLocation[][] grid)
+        {
+            if (y < 0 || y >= grid.Length)
+                return null;
+
+            var line = grid[y];
+            return x < 0 || x >= line.Length ? null : line[x];
+        }
+
+        public static IEnumerable<HeightmapLocation> GetAdjacentLocations(HeightmapLocation location, HeightmapLocation[][] grid) =>
+            PossibleMovements
+                .Select(m => (x: location.X + m.x, y: location.Y + m.y))
+                .Select(p => SafeGetLocation(p.x, p.y, grid))
+                .Where(x => x != null)
+                .Select(x => x!);
     }
 }
