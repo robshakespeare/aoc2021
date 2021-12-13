7ce5349b3b563a3f632fa6c78863683f0fda72d4
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-13 05:51:08 +0000

    Day 13, Part 1, done

--- AoC.Tests/Day13/Day13SolverTests.cs
+++ AoC.Tests/Day13/Day13SolverTests.cs
@@ -6,16 +6,60 @@ public class Day13SolverTests
 {
     private readonly Day13Solver _sut = new();
 
-    private const string ExampleInput = @"";
+    private const string ExampleInput = @"6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5";
 
     [Test]
-    public void Part1Example()
+    public void Part1Example1()
     {
         // ACT
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(17);
+    }
+
+    [Test]
+    public void Part1Example2()
+    {
+        var (grid, foldInstructions) = Day13Solver.Parse(ExampleInput);
+
+        // ACT
+        grid = Day13Solver.Fold(grid, foldInstructions.ElementAt(0));
+        grid = Day13Solver.Fold(grid, foldInstructions.ElementAt(1));
+
+        // ASSERT
+        grid.SelectMany(line => line).Count(chr => chr == Day13Solver.Dot).Should().Be(16);
+
+        grid.Should().BeEquivalentTo(new[]
+        {
+            "#####",
+            "#...#",
+            "#...#",
+            "#...#",
+            "#####",
+            ".....",
+            "....."
+        }, opts => opts.WithStrictOrdering());
     }
 
     [Test]
@@ -25,7 +69,8 @@ public class Day13SolverTests
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().NotBe(749);
+        part1Result.Should().Be(647);
     }
 
     [Test]
