dfa3ab1911dee57d6e8e52af468762402a7ac2d1
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-15 07:18:57 +0000

    Day 15, part 1, whoop, AStar worked, thank you https://cse442-17f.github.io/A-Star-Search-and-Dijkstras-Algorithm/

--- AoC/Day15/Day15Solver.cs
+++ AoC/Day15/Day15Solver.cs
@@ -10,7 +10,7 @@ public class Day15Solver : SolverBase
 
         //return GetDefaultPath(cavern).TotalRiskLevel;
 
-        var pathWithLowestRisk = GetPathWithLowestRisk(cavern);
+        var pathWithLowestRisk = AStarSearch(cavern);
 
         return pathWithLowestRisk.TotalRiskLevel;
 
@@ -24,7 +24,49 @@ public class Day15Solver : SolverBase
         return null;
     }
 
-    
+    // https://cse442-17f.github.io/A-Star-Search-and-Dijkstras-Algorithm/
+    private static Path AStarSearch(Cavern cavern)
+    {
+        var explore = new PriorityQueue<(Node Node, Path Path, int Cost), long>();
+        explore.Enqueue((cavern.Start, Path.Begin(cavern.Start), cavern.Start.RiskLevel), 0);
+
+        var seen = new HashSet<Node>();
+
+        while (explore.Count > 0)
+        {
+            var (node, path, cost) = explore.Dequeue(); // this takes out a node/state
+
+            // if node is the goal return the path
+            if (node == cavern.End)
+            {
+                return path;
+            }
+
+            // if not node in seen
+            if (!seen.Contains(node))
+            {
+                //for child, direction, stepcost in problem.getSuccessors(node):
+                foreach (var child in cavern.Grid.GetAdjacent(GridUtils.DirectionsExcludingDiagonal, node.Position))
+                {
+                    // explore.push((child, path + [direction], stepcost + cost), stepcost + cost + heuristic(child)) // the heursitic is added here as a part of the priority
+
+                    var stepcost = child.RiskLevel;
+
+                    // rs-todo: ideally change path.Visit(child) to path + child, and also I don't think we need to track VisitedNodes
+                    explore.Enqueue((child, path.Visit(child), stepcost + cost), stepcost + cost + heuristic(child, cavern.End));
+                }
+                seen.Add(node);
+            }
+        }
+
+        throw new InvalidOperationException("No paths found");
+    }
+
+    private static long heuristic(Node child, Node target)
+    {
+        return MathUtils.ManhattanDistance(child.Position, target.Position);
+    }
+
 
     private static Path GetPathWithLowestRisk(Cavern cavern)
     {
@@ -42,7 +84,7 @@ public class Day15Solver : SolverBase
             foreach (var currentPath in currentPaths)
             {
                 var adjacentLocations = cavern.Grid
-                    .GetAdjacent(GridUtils.DirectionsExcludingDiagonal, currentPath.CurrentLocation.Position)
+                    .GetAdjacent(GridUtils.DirectionsExcludingDiagonal, currentPath.CurrentNode.Position)
                     .Except(currentPath.VisitedLocations);
 
                 foreach (var adjacentLocation in adjacentLocations)
@@ -77,19 +119,26 @@ public class Day15Solver : SolverBase
         return completePaths.MinBy(x => x.TotalRiskLevel) ?? throw new InvalidOperationException("No paths found");
     }
 
-    public record Location(Vector2 Position, int RiskLevel)
+    //public record Node(Vector2 Position, int RiskLevel)
+    //{
+    //    public string Id { get; } = $"{Position.X},{Position.Y} ({RiskLevel})";
+
+    //    public override string ToString() => Id;
+    //}
+
+    public record Node(Vector2 Position, int RiskLevel)
     {
         public string Id { get; } = $"{Position.X},{Position.Y}:{RiskLevel}";
     }
 
-    public record Cavern(Location[][] Grid)
+    public record Cavern(Node[][] Grid)
     {
-        public Location Start { get; } = Grid[0][0];
-        public Location End { get; } = Grid.Last().Last();
+        public Node Start { get; } = Grid[0][0];
+        public Node End { get; } = Grid.Last().Last();
 
         public static Cavern Parse(PuzzleInput input) => new(input.ReadLines().Select(
             (line, y) => line.Select(
-                (c, x) => new Location(new Vector2(x, y), int.Parse(c.ToString()))).ToArray()).ToArray());
+                (c, x) => new Node(new Vector2(x, y), int.Parse(c.ToString()))).ToArray()).ToArray());
 
         public Path GetDefaultPath()
         {
@@ -100,22 +149,22 @@ public class Day15Solver : SolverBase
             for (var step = 1; step <= manhattanDistance; step++)
             {
                 var nextDirection = step % 2 == 0 ? MathUtils.South : MathUtils.East;
-                var nextPosition = path.CurrentLocation.Position + nextDirection;
+                var nextPosition = path.CurrentNode.Position + nextDirection;
                 var nextLocation = Grid.SafeGet(nextPosition) ?? throw new InvalidOperationException($"Next position {nextPosition} was invalid");
                 path = path.Visit(nextLocation);
             }
 
-            return path.CurrentLocation == End ? path : throw new InvalidOperationException("End not reached");
+            return path.CurrentNode == End ? path : throw new InvalidOperationException("End not reached");
         }
     }
 
     public class Path
     {
-        private Path(string name, int totalRiskLevel, Location currentLocation, IReadOnlySet<Location> visitedLocations)
+        private Path(string name, int totalRiskLevel, Node currentNode, IReadOnlySet<Node> visitedLocations)
         {
             Name = name;
             TotalRiskLevel = totalRiskLevel;
-            CurrentLocation = currentLocation;
+            CurrentNode = currentNode;
             VisitedLocations = visitedLocations;
         }
 
@@ -123,9 +172,9 @@ public class Day15Solver : SolverBase
 
         public int TotalRiskLevel { get; }
 
-        public Location CurrentLocation { get; }
+        public Node CurrentNode { get; }
 
-        public IReadOnlySet<Location> VisitedLocations { get; }
+        public IReadOnlySet<Node> VisitedLocations { get; }
 
         public override string ToString() => Name;
 
@@ -135,12 +184,12 @@ public class Day15Solver : SolverBase
 
         public override int GetHashCode() => Name.GetHashCode();
 
-        public Path Visit(Location location) => new(
-            Name + '>' + location.Id,
-            TotalRiskLevel + location.RiskLevel,
-            location,
-            new HashSet<Location>(VisitedLocations) {location});
+        public Path Visit(Node node) => new(
+            Name + '>' + node.Id,
+            TotalRiskLevel + node.RiskLevel,
+            node,
+            new HashSet<Node>(VisitedLocations) {node});
 
-        public static Path Begin(Location begin) => new(begin.Id, 0, begin, new HashSet<Location> { begin });
+        public static Path Begin(Node begin) => new(begin.Id, 0, begin, new HashSet<Node> { begin });
     }
 }
