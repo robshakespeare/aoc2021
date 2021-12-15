e69a68c979894950f7a98ebc679a9a10bca048ab
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-15 07:27:56 +0000

    Day 15, part 1, slightly tidier

--- AoC/Day15/Day15Solver.cs
+++ AoC/Day15/Day15Solver.cs
@@ -8,15 +8,11 @@ public class Day15Solver : SolverBase
     {
         var cavern = Cavern.Parse(input);
 
-        //return GetDefaultPath(cavern).TotalRiskLevel;
-
+        // Get the path with lowest risk, where risk is the cost in the A* search
+        // And the heuristic is simply the manhattan distance, i.e. the remaining amount of minimum steps, even at risk level 1, it will take to move from the node to the end
         var pathWithLowestRisk = AStarSearch(cavern);
 
         return pathWithLowestRisk.TotalRiskLevel;
-
-        //var pathWithLowestTotalRisk = completePaths.MinBy(x => x.TotalRiskLevel);
-
-        //return pathWithLowestTotalRisk?.TotalRiskLevel;
     }
 
     public override long? SolvePart2(PuzzleInput input)
@@ -24,7 +20,12 @@ public class Day15Solver : SolverBase
         return null;
     }
 
-    // https://cse442-17f.github.io/A-Star-Search-and-Dijkstras-Algorithm/
+    /// <summary>
+    /// Returns the path with the lowest total risk.
+    /// From: https://cse442-17f.github.io/A-Star-Search-and-Dijkstras-Algorithm/
+    /// rs-todo: needs more tidy
+    /// rs-todo: make this generic, so it can be used easily.
+    /// </summary>
     private static Path AStarSearch(Cavern cavern)
     {
         var explore = new PriorityQueue<(Node Node, Path Path, int Cost), long>();
@@ -53,7 +54,7 @@ public class Day15Solver : SolverBase
                     var stepcost = child.RiskLevel;
 
                     // rs-todo: ideally change path.Visit(child) to path + child, and also I don't think we need to track VisitedNodes
-                    explore.Enqueue((child, path.Visit(child), stepcost + cost), stepcost + cost + heuristic(child, cavern.End));
+                    explore.Enqueue((child, path.Visit(child), stepcost + cost), stepcost + cost + Heuristic(child, cavern.End));
                 }
                 seen.Add(node);
             }
@@ -62,73 +63,13 @@ public class Day15Solver : SolverBase
         throw new InvalidOperationException("No paths found");
     }
 
-    private static long heuristic(Node child, Node target)
-    {
-        return MathUtils.ManhattanDistance(child.Position, target.Position);
-    }
-
-
-    private static Path GetPathWithLowestRisk(Cavern cavern)
-    {
-        var currentPaths = new HashSet<Path>();
-        var completePaths = new List<Path>();
-
-        currentPaths.Add(Path.Begin(cavern.Start));
-
-        var lowestTotalRisk = cavern.GetDefaultPath().TotalRiskLevel;
-
-        while (currentPaths.Any())
-        {
-            var newPaths = new HashSet<Path>();
-
-            foreach (var currentPath in currentPaths)
-            {
-                var adjacentLocations = cavern.Grid
-                    .GetAdjacent(GridUtils.DirectionsExcludingDiagonal, currentPath.CurrentNode.Position)
-                    .Except(currentPath.VisitedLocations);
-
-                foreach (var adjacentLocation in adjacentLocations)
-                {
-                    var newPath = currentPath.Visit(adjacentLocation);
-
-                    if (adjacentLocation == cavern.End)
-                    {
-                        completePaths.Add(newPath);
-                        lowestTotalRisk = Math.Min(newPath.TotalRiskLevel, lowestTotalRisk);
-                    }
-                    else if (newPath.TotalRiskLevel < lowestTotalRisk)
-                    {
-                        var manhattanDistance = MathUtils.ManhattanDistance(adjacentLocation.Position, cavern.End.Position);
-
-                        var remainingMinRisk = lowestTotalRisk - newPath.TotalRiskLevel;
-
-                        var reject = newPath.TotalRiskLevel >= lowestTotalRisk ||
-                                     manhattanDistance > remainingMinRisk;
-
-                        if (!reject)
-                        {
-                            newPaths.Add(newPath);
-                        }
-                    }
-                }
-            }
-
-            currentPaths = newPaths;
-        }
-
-        return completePaths.MinBy(x => x.TotalRiskLevel) ?? throw new InvalidOperationException("No paths found");
-    }
-
-    //public record Node(Vector2 Position, int RiskLevel)
-    //{
-    //    public string Id { get; } = $"{Position.X},{Position.Y} ({RiskLevel})";
-
-    //    public override string ToString() => Id;
-    //}
+    private static long Heuristic(Node child, Node target) => MathUtils.ManhattanDistance(child.Position, target.Position);
 
     public record Node(Vector2 Position, int RiskLevel)
     {
         public string Id { get; } = $"{Position.X},{Position.Y}:{RiskLevel}";
+
+        public override string ToString() => Id;
     }
 
     public record Cavern(Node[][] Grid)
@@ -139,33 +80,15 @@ public class Day15Solver : SolverBase
         public static Cavern Parse(PuzzleInput input) => new(input.ReadLines().Select(
             (line, y) => line.Select(
                 (c, x) => new Node(new Vector2(x, y), int.Parse(c.ToString()))).ToArray()).ToArray());
-
-        public Path GetDefaultPath()
-        {
-            var manhattanDistance = MathUtils.ManhattanDistance(Start.Position, End.Position);
-
-            var path = Path.Begin(Start);
-
-            for (var step = 1; step <= manhattanDistance; step++)
-            {
-                var nextDirection = step % 2 == 0 ? MathUtils.South : MathUtils.East;
-                var nextPosition = path.CurrentNode.Position + nextDirection;
-                var nextLocation = Grid.SafeGet(nextPosition) ?? throw new InvalidOperationException($"Next position {nextPosition} was invalid");
-                path = path.Visit(nextLocation);
-            }
-
-            return path.CurrentNode == End ? path : throw new InvalidOperationException("End not reached");
-        }
     }
 
     public class Path
     {
-        private Path(string name, int totalRiskLevel, Node currentNode, IReadOnlySet<Node> visitedLocations)
+        private Path(string name, int totalRiskLevel, Node currentNode)
         {
             Name = name;
             TotalRiskLevel = totalRiskLevel;
             CurrentNode = currentNode;
-            VisitedLocations = visitedLocations;
         }
 
         public string Name { get; }
@@ -173,8 +96,6 @@ public class Day15Solver : SolverBase
         public int TotalRiskLevel { get; }
 
         public Node CurrentNode { get; }
-
-        public IReadOnlySet<Node> VisitedLocations { get; }
 
         public override string ToString() => Name;
 
@@ -187,9 +108,8 @@ public class Day15Solver : SolverBase
         public Path Visit(Node node) => new(
             Name + '>' + node.Id,
             TotalRiskLevel + node.RiskLevel,
-            node,
-            new HashSet<Node>(VisitedLocations) {node});
+            node);
 
-        public static Path Begin(Node begin) => new(begin.Id, 0, begin, new HashSet<Node> { begin });
+        public static Path Begin(Node begin) => new(begin.Id, 0, begin);
     }
 }
