d20f3a2a883cc8089aab71a4bb7539f2313e60a3
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-19 14:18:32 +0000

    Day 19, proper puzzled

--- AoC/Day19/Day19Solver.cs
+++ AoC/Day19/Day19Solver.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using MoreLinq;
 using static System.Environment;
 
@@ -15,50 +14,36 @@ public class Day19Solver : SolverBase
         // Any where there are 12 or more deltas that match, we should then be able to say they overlap and get the scanner's position
 
         // Challenge is basically given to sets of positions in local/relative space, which ones overlap, without knowing their positions in world space
-        // Min/mid bounds approach isn;t going to work, that relies of the set of positions being in the same grid
+        // Min/mid bounds approach isn't going to work, that relies of the set of positions being in the same grid
         // Brute force way is to work out the differences, and then any where 12 or more align, we have a matching "intersection"
 
         var scanners = Scanner.ParseInputToScanners(input);
 
-        //var scanner = scanners.First();
-        ////var scanner0LocalSpaceBeacons = new HashSet<Vector3>(scanner0.RelativeToBinBoundsBeaconPositions);
+        var scannerRelations = new Dictionary<int, (Vector3 RelativePosition, int ScannerId)>();
 
-        //foreach (var scanner2 in scanners)
-        //{
-        //    foreach (var otherScanner in scanners.Where(x => x != scanner2))
-        //    {
-        //        //foreach (var scannerOrientation in scanner2.GetOrientations())
-        //        //{
-        //            //Console.WriteLine($"--- Scanner {otherScanner.ScannerId} ---");
-        //            foreach (var otherScannerOrientation in otherScanner.GetOrientations())
-        //            {
-        //                var intersections = scanner2.RelativeToBinBoundsBeaconPositions.Intersect(otherScannerOrientation.RelativeToBinBoundsBeaconPositions).ToArray();
-        //                //Console.WriteLine($"Intersections: {intersections.Length}");
-        //                if (intersections.Length > 0) // >= 12) // rs-todo: should be 12!!!
-        //                {
-        //                    Console.WriteLine(
-        //                        $"Scanner {scanner2.ScannerId} has ({scanner2.RelativeToBinBoundsBeaconPositions.Count} {otherScannerOrientation.RelativeToBinBoundsBeaconPositions.Count}) {intersections.Length} matches other scanner {otherScannerOrientation.ScannerId} orientation {otherScannerOrientation.Orientation}");
-        //                }
-        //            }
+        foreach (var scanner in scanners)
+        {
+            foreach (var otherScanner in scanners.Where(x => x != scanner))
+            {
+                if (!scannerRelations.ContainsKey(otherScanner.ScannerId))
+                {
+                    var relativePosition = scanner.GetRelativePositionOfOtherScanner(otherScanner);
 
-        //            //Console.WriteLine();
-        //            //Console.WriteLine();
-        //        //}
-        //    }
-        //}
+                    if (relativePosition != null)
+                    {
+                        //Console.WriteLine($"Scanner {otherScanner.ScannerId} is at {relativePosition} (relative to scanner {scanner.ScannerId}).");
+                        scannerRelations[otherScanner.ScannerId] = (relativePosition.Value, scanner.ScannerId);
+                    }
+                }
 
-        
+                //break;
+            }
+        }
 
-        ////var points = new Vector3(404, -588, -901);
-
-        ////var perms = GetAllPermutations(points).Select(x => string.Join(", ", x)).Distinct().ToArray();
-
-        ////Console.WriteLine($"There are {perms.Length} perms:");
-
-        ////foreach (var perm in perms)
-        ////{
-        ////    Console.WriteLine(perm);
-        ////}
+        foreach (var (otherScannerId, (relativePosition, scannerId)) in scannerRelations)
+        {
+            Console.WriteLine($"Scanner {otherScannerId} is at {relativePosition} (relative to scanner {scannerId}).");
+        }
 
         return null;
     }
@@ -68,40 +53,21 @@ public class Day19Solver : SolverBase
         return null;
     }
 
-    //public readonly record struct Vector3(int X, int Y, int Z)
-    //{
-    //    public static Vector3 operator -(Vector3 left, Vector3 right) => new(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
-    //}
-
     public class Scanner
     {
         public int ScannerId { get; }
 
         public IReadOnlyList<Vector3> Beacons { get; }
 
-        /// <summary>
-        /// rs-todo: is this comment going to be correct?
-        /// The orientation of the scanner. In total, each scanner could be in any of 24 different orientations.
-        /// Orientation 0 is the original orientation.
-        /// </summary>
         public int Orientation { get; }
 
         public Lazy<IReadOnlyList<Scanner>> AllOrientations { get; }
-
-        ////public Vector3 MinBounds { get; }
-
-        ////public IReadOnlyList<Vector3> RelativeToBinBoundsBeaconPositions { get; set; }
-
-        ////public Vector3? Position { get; set; } = null;
 
         public Scanner(int scannerId, IReadOnlyList<Vector3> beacons, bool baseOrientation, int orientation = 0)
         {
             ScannerId = scannerId;
             Beacons = beacons;
             Orientation = orientation;
-            ////MinBounds = new Vector3(beacons.Min(b => b.X), beacons.Min(b => b.Y), beacons.Min(b => b.Z));  //beacons.Aggregate(Vector3.Min);
-            ////RelativeToBinBoundsBeaconPositions = beacons.Select(beacon => beacon - MinBounds).ToArray();
-
             AllOrientations = new Lazy<IReadOnlyList<Scanner>>(
                 () => baseOrientation ? GetOrientations() : throw new InvalidOperationException("Should only get orientations of base orientation"));
         }
@@ -140,9 +106,6 @@ public class Day19Solver : SolverBase
         public IEnumerable<(Vector3 Beacon, Vector3 Delta)> GetDeltasToBeacon(Vector3 endBeacon) =>
             Beacons.Select(startBeacon => (startBeacon, endBeacon - startBeacon));
 
-        /// <summary>
-        /// In total, each scanner could be in any of 24 different orientations. // rs-todo: is comment correct?
-        /// </summary>
         private IReadOnlyList<Scanner> GetOrientations()
         {
             // Top level array is the length of the original number of Beacons
@@ -171,23 +134,9 @@ public class Day19Solver : SolverBase
 
         private static Func<Vector3, Vector3>[] BuildPermutors()
         {
-            // rs-todo: use a delta?
-            float Fix(float f) => f == -0f ? 0 : f;
-
+            static float Fix(float f) => Math.Abs(f - (-0f)) < 0.0001 ? 0 : f;
             return new[]
                 {
-                    //new Func<Vector3, int>[] {v => v.X, v => v.Y, v => v.Z},
-                    //new Func<Vector3, int>[] {v => -v.X, v => -v.Y, v => -v.Z},
-
-                    //new Func<Vector3, int>[] {v => -v.X, v => v.Y, v => v.Z},
-                    //new Func<Vector3, int>[] {v => v.X, v => -v.Y, v => v.Z},
-                    //new Func<Vector3, int>[] {v => v.X, v => v.Y, v => -v.Z},
-
-                    //new Func<Vector3, int>[] {v => -v.X, v => -v.Y, v => v.Z},
-                    //new Func<Vector3, int>[] {v => v.X, v => -v.Y, v => -v.Z},
-
-                    //new Func<Vector3, int>[] {v => -v.X, v => v.Y, v => -v.Z}
-
                     new Func<Vector3, float>[] {v => v.X, v => v.Y, v => v.Z},
                     new Func<Vector3, float>[] {v => Fix(-v.X), v => Fix(-v.Y), v => Fix(-v.Z)},
 
