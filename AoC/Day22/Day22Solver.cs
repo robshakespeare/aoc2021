4d4190b654a1dee38e7adf983b385993d0c56f30
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-22 05:41:14 +0000

    Day 22, d'oh, should have known this was going to be too slow!

--- AoC/Day22/Day22Solver.cs
+++ AoC/Day22/Day22Solver.cs
@@ -2,15 +2,84 @@ namespace AoC.Day22;
 
 public class Day22Solver : SolverBase
 {
-    public override string DayName => "";
+    public override string DayName => "Reactor Reboot";
 
     public override long? SolvePart1(PuzzleInput input)
     {
-        return null;
+        var activeCubes = new HashSet<Vector3>();
+        var rebootSteps = ParseInput(input);
+        var outerBounds = new Bounds(new Vector3(-50, -50, -50), new Vector3(50, 50, 50));
+
+        foreach (var (isSet, bounds) in rebootSteps)
+        {
+            foreach (var position in bounds.GetPositionsWithinBoundsAndWithinOuterBounds(outerBounds))
+            {
+                if (isSet)
+                {
+                    activeCubes.Add(position);
+                }
+                else
+                {
+                    activeCubes.Remove(position);
+                }
+            }
+        }
+
+        return activeCubes.Count;
     }
 
     public override long? SolvePart2(PuzzleInput input)
     {
         return null;
+    }
+
+    private static readonly Regex ParseInputRegex = new(
+        @"(?<onOrOff>on|off) x=(?<x1>-?\d+)\.\.(?<x2>-?\d+),y=(?<y1>-?\d+)\.\.(?<y2>-?\d+),z=(?<z1>-?\d+)\.\.(?<z2>-?\d+)", RegexOptions.Compiled);
+
+    public static IReadOnlyList<RebootStep> ParseInput(PuzzleInput input) => ParseInputRegex.Matches(input.ToString()).Select(match =>
+    {
+        var on = match.Groups["onOrOff"].Value == "on";
+        var x1 = int.Parse(match.Groups["x1"].Value);
+        var x2 = int.Parse(match.Groups["x2"].Value);
+        var y1 = int.Parse(match.Groups["y1"].Value);
+        var y2 = int.Parse(match.Groups["y2"].Value);
+        var z1 = int.Parse(match.Groups["z1"].Value);
+        var z2 = int.Parse(match.Groups["z2"].Value);
+
+        // Coordinate space shouldn't matter, but for the sake of ease mental visualization, +ve X RIGHT, +ve Y DOWN, +ve Z BACK
+        return new RebootStep(
+            on,
+            new Bounds(
+                LowerBounds: new Vector3(Math.Min(x1, x2), Math.Min(y1, y2), Math.Min(z1, z2)),
+                UpperBounds: new Vector3(Math.Max(x1, x2), Math.Max(y1, y2), Math.Max(z1, z2))));
+    }).ToArray();
+
+    public record RebootStep(bool IsSet, Bounds Bounds)
+    {
+
+    }
+
+    public record Bounds(Vector3 LowerBounds, Vector3 UpperBounds)
+    {
+        public bool Contains(Vector3 position) =>
+            LowerBounds.X >= position.X && LowerBounds.Y >= position.Y && LowerBounds.Z >= position.Z &&
+            UpperBounds.X <= position.X && UpperBounds.Y <= position.Y && UpperBounds.Z <= position.Z;
+
+        public IEnumerable<Vector3> GetPositionsWithinBounds()
+        {
+            for (var z = (int) LowerBounds.Z; z <= (int) UpperBounds.Z; z++)
+            {
+                for (var y = (int) LowerBounds.Y; y <= (int) UpperBounds.Y; y++)
+                {
+                    for (var x = (int) LowerBounds.X; x <= (int) UpperBounds.X; x++)
+                    {
+                        yield return new Vector3(x, y, z);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Vector3> GetPositionsWithinBoundsAndWithinOuterBounds(Bounds outerBounds) =>
+            GetPositionsWithinBounds().Where(outerBounds.Contains);
     }
 }
