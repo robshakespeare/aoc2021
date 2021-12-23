180ac961532945be97f4cb45759cd264f37a0dec
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-23 07:57:38 +0000

    Day 23 part 1 solved in a very basic way!

--- AoC/Day23/Day23Solver.cs
+++ AoC/Day23/Day23Solver.cs
@@ -2,12 +2,9 @@ namespace AoC.Day23;
 
 public class Day23Solver : SolverBase
 {
-    public override string DayName => "";
+    public override string DayName => "Amphipod";
 
-    public override long? SolvePart1(PuzzleInput input)
-    {
-        return null;
-    }
+    public override long? SolvePart1(PuzzleInput input) => new Day23Part1Solver(input).Solve();
 
     public override long? SolvePart2(PuzzleInput input)
     {
