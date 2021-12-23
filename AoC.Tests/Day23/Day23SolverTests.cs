dfc87883bac39230b4638ee92bf6b34e57b8a761
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-23 07:57:38 +0000

    Day 23 part 1 solved in a very basic way!

--- AoC.Tests/Day23/Day23SolverTests.cs
+++ AoC.Tests/Day23/Day23SolverTests.cs
@@ -9,23 +9,13 @@ public class Day23SolverTests
     private const string ExampleInput = @"";
 
     [Test]
-    public void Part1Example()
-    {
-        // ACT
-        var part1ExampleResult = _sut.SolvePart1(ExampleInput);
-
-        // ASSERT
-        part1ExampleResult.Should().Be(null);
-    }
-
-    [Test]
     public void Part1ReTest()
     {
         // ACT
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().Be(11536);
     }
 
     [Test]
