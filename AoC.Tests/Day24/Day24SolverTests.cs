2b8f37a13fa32f9598dc5f01efdc76e818854152
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-12-24 06:54:35 +0000

    Day 24, ALU built, now what

--- AoC.Tests/Day24/Day24SolverTests.cs
+++ AoC.Tests/Day24/Day24SolverTests.cs
@@ -1,4 +1,5 @@
 using AoC.Day24;
+using static AoC.Day24.Day24Solver;
 
 namespace AoC.Tests.Day24;
 
@@ -6,17 +7,105 @@ public class Day24SolverTests
 {
     private readonly Day24Solver _sut = new();
 
-    private const string ExampleInput = @"";
-
-    [Test]
-    public void Part1Example()
+    private static Func<int> Inputs(params int[] inputs)
     {
+        var queue = new Queue<int>(inputs);
+        return () => queue.Dequeue();
+    }
+
+    [TestCase(10, -10)]
+    [TestCase(-10, 10)]
+    [TestCase(65, -65)]
+    [TestCase(-65, 65)]
+    public void Program_TakesInput_Negates_StoresInZ_Example_Test(int input, int expectedX)
+    {
+        var sut = Program.Parse(@"inp x
+mul x -1");
+
         // ACT
-        var part1ExampleResult = _sut.SolvePart1(ExampleInput);
+        var state = sut.Execute(Inputs(input));
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        using (new AssertionScope())
+        {
+            state.W.Should().Be(0);
+            state.X.Should().Be(expectedX);
+            state.Y.Should().Be(0);
+            state.Z.Should().Be(0);
+        }
     }
+
+    [TestCase(1, 3, 1)]
+    [TestCase(1, 4, 0)]
+    [TestCase(7, 21, 1)]
+    [TestCase(7, 20, 0)]
+    public void Program_Takes2Inputs_StoresInZWhetherSecondInputIsThreeTimesLargerThanFirstInput_Example_Test(int input1, int input2, int expectedZ)
+    {
+        var sut = Program.Parse(@"inp z
+inp x
+mul z 3
+eql z x");
+
+        // ACT
+        var state = sut.Execute(Inputs(input1, input2));
+
+        // ASSERT
+        using (new AssertionScope())
+        {
+            state.W.Should().Be(0);
+            state.X.Should().Be(input2);
+            state.Y.Should().Be(0);
+            state.Z.Should().Be(expectedZ);
+        }
+    }
+
+    [TestCase(0, 0, 0, 0, 0)]
+    [TestCase(1, 0, 0, 0, 1)]
+    [TestCase(2, 0, 0, 1, 0)]
+    [TestCase(4, 0, 1, 0, 0)]
+    [TestCase(8, 1, 0, 0, 0)]
+    [TestCase(3, 0, 0, 1, 1)]
+    [TestCase(6, 0, 1, 1, 0)]
+    [TestCase(11, 1, 0, 1, 1)]
+    [TestCase(15, 1, 1, 1, 1)]
+    public void Program_BinConvertExample_Test(int input, int expectedW, int expectedX, int expectedY, int expectedZ)
+    {
+        var sut = Program.Parse(@"inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2");
+
+        // ACT
+        var state = sut.Execute(Inputs(input));
+
+        // ASSERT
+        using (new AssertionScope())
+        {
+            state.W.Should().Be(expectedW);
+            state.X.Should().Be(expectedX);
+            state.Y.Should().Be(expectedY);
+            state.Z.Should().Be(expectedZ);
+        }
+    }
+
+    //private const string ExampleInput = @"";
+
+    //[Test]
+    //public void Part1Example()
+    //{
+    //    // ACT
+    //    var part1ExampleResult = _sut.SolvePart1(ExampleInput);
+
+    //    // ASSERT
+    //    part1ExampleResult.Should().Be(null);
+    //}
 
     [Test]
     public void Part1ReTest()
@@ -28,15 +117,15 @@ public class Day24SolverTests
         part1Result.Should().Be(null);
     }
 
-    [Test]
-    public void Part2Example()
-    {
-        // ACT
-        var part2ExampleResult = _sut.SolvePart2(ExampleInput);
+    //[Test]
+    //public void Part2Example()
+    //{
+    //    // ACT
+    //    var part2ExampleResult = _sut.SolvePart2(ExampleInput);
 
-        // ASSERT
-        part2ExampleResult.Should().Be(null);
-    }
+    //    // ASSERT
+    //    part2ExampleResult.Should().Be(null);
+    //}
 
     [Test]
     public void Part2ReTest()
--- AoC/Day24/Day24Solver.cs
+++ AoC/Day24/Day24Solver.cs
@@ -2,7 +2,7 @@ namespace AoC.Day24;
 
 public class Day24Solver : SolverBase
 {
-    public override string DayName => "";
+    public override string DayName => "Arithmetic Logic Unit";
 
     public override long? SolvePart1(PuzzleInput input)
     {
@@ -12,5 +12,163 @@ public class Day24Solver : SolverBase
     public override long? SolvePart2(PuzzleInput input)
     {
         return null;
+    }
+
+    public class Program
+    {
+        private readonly IReadOnlyList<IInstruction> _instructions;
+
+        public Program(IReadOnlyList<IInstruction> instructions)
+        {
+            _instructions = instructions;
+        }
+
+        public ProgramState Execute(Func<int> getNextInput)
+        {
+            var state = new ProgramState();
+
+            foreach (var instruction in _instructions)
+            {
+                instruction.Execute(state, getNextInput);
+            }
+
+            return state;
+        }
+
+        public static Program Parse(PuzzleInput input) => new(input.ReadLines().Select(line =>
+        {
+            var parts = line.Split(' ');
+            var ins = parts[0];
+            var a = parts[1].Single();
+            var b = () => new BPart(parts[2]);
+
+            return (IInstruction) (ins switch
+            {
+                "inp" => new InputInstruction(a),
+                "add" => new AddInstruction(a, b()),
+                "mul" => new MultiplyInstruction(a, b()),
+                "div" => new DivideInstruction(a, b()),
+                "mod" => new ModInstruction(a, b()),
+                "eql" => new EqualsInstruction(a, b()),
+                _ => throw new InvalidOperationException("Invalid instruction line: " + line)
+            });
+        }).ToArray());
+    }
+
+    public interface IInstruction
+    {
+        void Execute(ProgramState state, Func<int> getNextInput);
+    }
+
+    public record InputInstruction(char A) : IInstruction
+    {
+        public void Execute(ProgramState state, Func<int> getNextInput) => state[A] = getNextInput();
+
+        public override string ToString() => $"Read an input value and write it to variable {A}.";
+    }
+
+    public record AddInstruction(char A, BPart B) : IInstruction
+    {
+        public void Execute(ProgramState state, Func<int> getNextInput) => state[A] += B.GetValue(state);
+
+        public override string ToString() => $"Add the value of variable {A} to the value of {B}, then store the result in variable {A}.";
+    }
+
+    public record MultiplyInstruction(char A, BPart B) : IInstruction
+    {
+        public void Execute(ProgramState state, Func<int> getNextInput) => state[A] *= B.GetValue(state);
+
+        public override string ToString() => $"Multiply the value of variable {A} by the value of {B}, then store the result in variable {A}.";
+    }
+
+    public record DivideInstruction(char A, BPart B) : IInstruction
+    {
+        public void Execute(ProgramState state, Func<int> getNextInput) => state[A] /= B.GetValue(state);
+
+        public override string ToString() =>
+            $"Divide the value of variable {A} by the value of {B}, truncate the result to an integer, then store the result in variable {A}.";
+    }
+
+    public record ModInstruction(char A, BPart B) : IInstruction
+    {
+        public void Execute(ProgramState state, Func<int> getNextInput) => state[A] %= B.GetValue(state);
+
+        public override string ToString() =>
+            $"(Mod instruction) Divide the value of variable {A} by the value of {B}, then store the remainder in variable {A}.";
+    }
+
+    public record EqualsInstruction(char A, BPart B) : IInstruction
+    {
+        public void Execute(ProgramState state, Func<int> getNextInput)
+        {
+            int a = state[A];
+            int b = B.GetValue(state);
+            state[A] = a == b ? 1 : 0;
+        }
+
+        public override string ToString() =>
+            $"If the value of variable {A} and {B} are equal, then store the value 1 in variable {A}. Otherwise, store the value 0 in variable {A}.";
+    }
+
+    public record BPart
+    {
+        public string B { get; }
+        public bool IsNumber { get; }
+        public int Number { get; }
+        public char VariableName { get; }
+
+        public BPart(string b)
+        {
+            B = b;
+            IsNumber = int.TryParse(b, out var num);
+            Number = num;
+            VariableName = !IsNumber ? b.Single() : default;
+        }
+
+        public int GetValue(ProgramState state) => IsNumber ? Number : state[VariableName];
+
+        public override string ToString() => IsNumber ? Number.ToString() : $"variable {VariableName}";
+    }
+
+    public class ProgramState
+    {
+        public int W { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public int this[char variableName]
+        {
+            get => variableName switch
+            {
+                'w' => W,
+                'x' => X,
+                'y' => Y,
+                'z' => Z,
+                _ => throw new InvalidOperationException("Unable to get variable. Invalid variable: " + variableName)
+            };
+            set
+            {
+                switch (variableName)
+                {
+                    case 'w':
+                        W = value;
+                        break;
+                    case 'x':
+                        X = value;
+                        break;
+                    case 'y':
+                        Y = value;
+                        break;
+                    case 'z':
+                        Z = value;
+                        break;
+                    default:
+                        throw new InvalidOperationException("Unable to set variable. Invalid variable: " + variableName);
+                }
+            }
+        }
+
+        public override string ToString() => $"w: {W}, x: {X}, y: {Y}, z: {Z}";
     }
 }
